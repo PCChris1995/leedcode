810cac9345aaa27d5c20c83e5f0581c8391dc0c4
PCChris1995 <pchen95@126.com>
2018-03-27 13:04:01 +0800

    modify test.py

--- test.py
+++ test.py
@@ -1,61 +1,55 @@
 class YahooDailyReader():
-    
     def __init__(self, symbol=None, start=None, end=None):
         import datetime, time
         self.symbol = symbol
-        
+
         # initialize start/end dates if not provided
         if end is None:
             end = datetime.datetime.today()
         if start is None:
-            start = datetime.datetime(2010,1,1)
-        
+            start = datetime.datetime(2010, 1, 1)
+
         self.start = start
         self.end = end
-        
+
         # convert dates to unix time strings
         unix_start = int(time.mktime(self.start.timetuple()))
         day_end = self.end.replace(hour=23, minute=59, second=59)
         unix_end = int(time.mktime(day_end.timetuple()))
-        
+
         url = 'https://finance.yahoo.com/quote/{}/history?'
         url += 'period1={}&period2={}'
         url += '&filter=history'
         url += '&interval=1d'
         url += '&frequency=1d'
         self.url = url.format(self.symbol, unix_start, unix_end)
-        
+
     def read(self):
         import requests, re, json
         import pandas as pd
-       
+
         r = requests.get(self.url)
-        
+
         ptrn = r'root\.App\.main = (.*?);\n}\(this\)\);'
         txt = re.search(ptrn, r.text, re.DOTALL).group(1)
         jsn = json.loads(txt)
-        df = pd.DataFrame(
-                jsn['context']['dispatcher']['stores']
-                ['HistoricalPriceStore']['prices']
-                )
+        df = pd.DataFrame(jsn['context']['dispatcher']['stores'][
+            'HistoricalPriceStore']['prices'])
         df.insert(0, 'symbol', self.symbol)
         df['date'] = pd.to_datetime(df['date'], unit='s').dt.date
-        
+
         # drop rows that aren't prices
         df = df.dropna(subset=['close'])
-        
-        df = df[['symbol', 'date', 'high', 'low', 'open', 'close', 
-                 'volume', 'adjclose']]
+
+        df = df[[
+            'symbol', 'date', 'high', 'low', 'open', 'close', 'volume',
+            'adjclose'
+        ]]
         df = df.set_index('symbol')
         return df
-    
 
 
-  
-        
-def insert_daily_data_into_db(
-        data_vendor_id, symbol_id, daily_data
-    ):
+def insert_daily_data_into_db(data_vendor_id, symbol_id, daily_data):
     """
     Takes a list of tuples of daily data and adds it to the
     MySQL database. Appends the vendor ID and symbol ID to the data.
@@ -76,10 +70,9 @@ def insert_daily_data_into_db(
     now = now.strftime("%Y-%m-%d")
 
     # Amend the data to include the vendor ID and symbol ID
-    daily_data = [
-        (data_vendor_id, symbol_id, daily_data[0], now, now,
-        daily_data[1], daily_data[2], daily_data[3], daily_data[4], daily_data[5], daily_data[6]) 
-    ]
+    daily_data = [(data_vendor_id, symbol_id, daily_data[0], now, now,
+                   daily_data[1], daily_data[2], daily_data[3], daily_data[4],
+                   daily_data[5], daily_data[6])]
 
     # Create the insert strings
     column_str = """data_vendor_id, symbol_id, price_date, created_date, 
@@ -90,26 +83,34 @@ def insert_daily_data_into_db(
         (column_str, insert_str)
 
     # Using the MySQL connection, carry out an INSERT INTO for every symbol
-    with con: 
+    with con:
         cur = con.cursor()
         cur.executemany(final_str, daily_data)
+
+
 def obtain_list_of_db_tickers():
     """
     Obtains a list of the ticker symbols in the database.
     """
-    with con: 
+    with con:
         cur = con.cursor()
         cur.execute("SELECT id, ticker FROM symbol")
         data = cur.fetchall()
         return [(d[0], d[1]) for d in data]
-        
+
 
 ydr = YahooDailyReader('BABA')
 df = ydr.read()
 for i in range(len(df)):
-    datestring = df.iloc[i,0]
+    datestring = df.iloc[i, 0]
     datestring = datestring.strftime("%Y-%m-%d")
-    dr = [datestring,float(df.iloc[i,1]),float(df.iloc[i,2]),float(df.iloc[i,3]),float(df.iloc[i,4]),float(df.iloc[i,5]),float(df.iloc[i,6])]
+    dr = [
+        datestring,
+        float(df.iloc[i, 1]),
+        float(df.iloc[i, 2]),
+        float(df.iloc[i, 3]),
+        float(df.iloc[i, 4]),
+        float(df.iloc[i, 5]),
+        float(df.iloc[i, 6])
+    ]
     insert_daily_data_into_db('1', '1', dr)
-
-print("test modify")
